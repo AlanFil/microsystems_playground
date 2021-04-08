ad8bd261cb34494f4671161afd059eca5cb6408f
AlanF <maildoalana@gmail.com>
2021-04-08 21:28:57 +0200

    api upgrade

--- consumer.py
+++ consumer.py
@@ -1,4 +1,12 @@
 import pika
+import json
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
+django.setup()
+
+from products.models import Product
 
 params = pika.URLParameters('amqps://zobyksdf:tEXN4vKXrQskmY9Lmi39zqk5IcdHzkSE@kangaroo.rmq.cloudamqp.com/zobyksdf')
 
@@ -11,10 +19,15 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, propreties, body):
     print('Received in admin')
-    print(body)
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Products liked increased!')
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
--- products/producer.py
+++ products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://zobyksdf:tEXN4vKXrQskmY9Lmi39zqk5IcdHzkSE@kangaroo.rmq.cloudamqp.com/zobyksdf')
 
@@ -7,5 +7,6 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish():  # method, body
-    channel.basic_publish(exchange='', routing_key='main', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
